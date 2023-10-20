618a8951ee0639df325ca84bdeaa783e7512677a
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-10-20 22:09:56 +0530

    updated code

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -10,7 +10,7 @@ const {
   varifySession,
   changedPassword,
   forgetPassword,
-  accountConfirm,getProfile
+  accountConfirm,getProfile,getRefreshToken
 } = require("../../controller/authentication/auth");
 const UpdatebyMiddleWare = require("../../middleware/updatedBy");
 const createMiddleWare = require("../../middleware/createMiddleWare");
@@ -31,6 +31,7 @@ authRoute.route("/user/auth/register").post(validateSignUpRequest, isRequestVali
 authRoute.route("/user/auth/confirm-account/:token").post(accountConfirm);
 authRoute.route("/user/auth/login").post(validateSignIpRequest, isRequestValidated,signIn);
 authRoute.route("/user/auth/verify/session").post(varifySession);
+authRoute.route("/user/auth/session/refresh").post(getRefreshToken);
 authRoute.route("/user/auth/reset-password/:token").post(validateResetpassword,isRequestValidated,resetPassword);
 authRoute.route("/user/auth/forget-password").post(validateForgetPassword,isRequestValidated,forgetPassword);
 authRoute.route("/user/auth/change-password").post(userMiddleWare,UpdatebyMiddleWare,validateChangePassword,isRequestValidated,changedPassword);
