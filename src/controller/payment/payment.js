7d764db471f6638cf9809bd41d3cceb2d54f8674
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-11-30 22:04:10 +0530

    updated

--- src/controller/payment/payment.js
+++ src/controller/payment/payment.js
@@ -5,6 +5,8 @@ const {
   getStatusCode,
 } = require("http-status-codes");
 const { FilterOptions } = require("../../utils/helper");
+const Razorpay = require('razorpay');
+
 const paypal = require("paypal-rest-sdk");
 const {
   jwtSecret,
@@ -29,51 +31,130 @@ paypal.configure({
 });
 
 const processPayment = async (req, res) => {
+  try {
+    // Validate the request body
 
-    try {
-        // Validate the request body
-     
-    
-        // Calculate subTotal and fetch product details
-        const productsWithDetails = await Promise.all(req.body.products.map(async (item) => {
-          const productDetails = await Product.findById(item.product);
-          if (!productDetails) {
-            throw new Error(`Product with ID ${item.product} not found.`);
-          }
-          return {
-            product: productDetails,
-            quantity: item.quantity,
-          };
-        }));
-    
-        const subTotal = productsWithDetails.reduce((total, item) => {
-          return total + item.product.price * item.quantity;
-        }, 0);
-    
-        // Add shipping charges (adjust as needed)
-        const shippingCharges = 5; // Assuming $5 for shipping
-        const total = subTotal + shippingCharges;
-    
-        // Create the order record
-        const order = new Order({
-          user: new mongoose.Types.ObjectId(), // Set the user ID based on your authentication
-          ...req.body,
-          products: productsWithDetails,
-          subTotal,
-          total,
-        });
-    
-        // Save the order to the database
-        await order.save();
-    
-        res.status(201).json(order);
-      } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+    // Calculate subTotal and fetch product details
+    const productsWithDetails = await Promise.all(req.body.products.map(async (item) => {
+      const productDetails = await Product.findById(item.product);
+      if (!productDetails) {
+        throw new Error(`Product with ID ${item.product} not found.`);
       }
+      return {
+        product: productDetails,
+        quantity: item.quantity,
+      };
+    }));
 
+    const subTotal = productsWithDetails.reduce((total, item) => {
+      return total + item.product.price * item.quantity;
+    }, 0);
 
+    // Add shipping charges (adjust as needed)
+    const shippingCharges = 5; // Assuming $5 for shipping
+    const total = subTotal + shippingCharges;
+
+    // Determine the payment method from the request body
+    const paymentMethod = req.body.payment_method;
+
+    let paymentResult;
+
+    if (paymentMethod === 'paypal') {
+      // Implement PayPal payment logic here
+      // Replace the following line with actual PayPal integration code
+      paymentResult = await payWithPayPal(total);
+    } else if (paymentMethod === 'razorpay') {
+      // Implement Razor Pay payment logic here
+      // Replace the following line with actual Razor Pay integration code
+      paymentResult = await payWithRazorPay(total);
+    } else {
+      throw new Error('Invalid payment method');
+    }
+
+    // If payment is successful, create the order record
+    if (paymentResult.success) {
+      const order = new Order({
+        user: new mongoose.Types.ObjectId(), // Set the user ID based on your authentication
+        ...req.body,
+        products: productsWithDetails,
+        subTotal,
+        total,
+      });
+
+      // Save the order to the database
+      await order.save();
+
+      res.status(201).json(order);
+    } else {
+      // Handle payment failure
+      res.status(400).json({ error: 'Payment failed' });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
+
+// Replace the following functions with actual PayPal and Razor Pay integration logic
+async function payWithPayPal(amount) {
+
+  const environment = new paypal.core.SandboxEnvironment(paypalClient, paypalSecret);
+const client = new paypal.core.PayPalHttpClient(environment);
+
+  try {
+    // Create an order request
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.prefer("return=representation");
+    request.requestBody({
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: amount.toString(),
+          },
+        },
+      ],
+    });
+
+    // Create the order
+    const response = await client.execute(request);
+
+    // Capture the order for payment
+    const captureRequest = new paypal.orders.OrdersCaptureRequest(response.result.id);
+    captureRequest.requestBody({});
+
+    const captureResponse = await client.execute(captureRequest);
+
+    // Return success along with PayPal payment ID
+    return { success: true, paymentId: captureResponse.result.id };
+  } catch (error) {
+    console.error('PayPal error:', error.message);
+    return { success: false };
+  }
+}
+
+async function payWithRazorPay(amount) {
+
+  const razorpay = new Razorpay({
+    key_id: 'YOUR_RAZORPAY_KEY_ID',
+    key_secret: 'YOUR_RAZORPAY_KEY_SECRET',
+  });
+
+  try {
+    // Create a Razor Pay order
+    const order = await razorpay.orders.create({
+      amount: amount * 100, // Razor Pay amount is in paisa, so multiply by 100
+      currency: 'INR', // Adjust currency as needed
+    });
+
+    // Return success along with Razor Pay payment ID
+    return { success: true, paymentId: order.id };
+  } catch (error) {
+    console.error('Razor Pay error:', error.message);
+    return { success: false };
+  }
+}
 
 module.exports = {
   processPayment,
