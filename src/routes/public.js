25a964e7165a5dfe6b643e7d362620309bf22168
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-12-25 17:18:57 +0530

    updated

--- src/routes/public.js
+++ src/routes/public.js
@@ -3,7 +3,7 @@ var session = require("express-session");
 const publicRoute = express.Router();
 
 const {
-    gethomeDetails,getSingleProductDetails,getProductsSearch,publicCategoriesDetails,getPublicBrands
+    gethomeDetails,getSingleProductDetails,getProductsSearch,publicCategoriesDetails,getPublicBrands,getAllTage
 } = require("../controller/public/home");
 
 publicRoute.route("/public/home/data").get(gethomeDetails);
@@ -12,6 +12,6 @@ publicRoute.route("/public/product/details").get(getSingleProductDetails);
 publicRoute.route("/public/product/search").get(getProductsSearch);
 publicRoute.route("/public/categories").get(publicCategoriesDetails);
 publicRoute.route("/public/brands").get(getPublicBrands);
-
+publicRoute.route("/public/tags").get(getAllTage);
 
 module.exports = publicRoute;
